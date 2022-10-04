2b59407c39c5b4755d8ed5ce3305c553bc310b03
VijayKumarDVK96 <43247564+VijayKumarDVK96@users.noreply.github.com>
2022-10-04 07:42:15 +0530

    movie price change display

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const moviesEl = document.querySelector('#movies');
+
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+
+// console.log(seats);
+
+const countEl = document.querySelector('#count');
+const totalEl = document.querySelector('#total');
+
+// console.log(countEl);
+// console.log(totalEl);
+
+let moviePrice = moviesEl.value;
+// console.log(moviePrice);
+
+moviesEl.addEventListener('change', function() {
+    moviePrice = moviesEl.value;
+    console.log(moviePrice);
+});
